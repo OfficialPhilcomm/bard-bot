0ccf5bcbcc76dbc03f455f2b8ebeb65a08eccac2
Philipp Schlesinger <info@philcomm.dev>
2023-12-04 22:24:16 +0100

    Remove unused config stuff

--- lib/config.rb
+++ lib/config.rb
@@ -11,16 +11,6 @@ module DndMusic
         integer :application_id
         string :public_key
         string :token
-        list :allowed_servers
-      end
-
-      group :esrgan do
-        string :home
-      end
-
-      group :webserver do
-        string :home
-        string :domain
       end
     end
   end
