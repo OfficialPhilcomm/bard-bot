b590a694638a3d35fdcb6284375f574fbdf2c9f6
Philipp Schlesinger <info@philcomm.dev>
2023-12-04 16:02:33 +0100

    Improve sound management

---
+++ lib/soundboard_manager.rb
@@ -0,0 +1,59 @@
+require_relative "sound"
+
+class SoundboardManager
+  def initialize(bot:)
+    @bot = bot
+    @sound_lists_per_server = {}
+    @currently_playing = {}
+    @switching_sounds = []
+  end
+
+  def play_sound(server_id, sound)
+    @bot.voices[server_id].play_file(sound.file)
+    @currently_playing[server_id] = sound
+  end
+
+  def stop(server_id)
+    @currently_playing.delete(server_id)
+    @bot.voices[server_id]&.stop_playing
+  end
+
+  def set_soundboard(server_id, sounds)
+    @sound_lists_per_server[server_id] = sounds
+  end
+
+  def get_soundboard(server_id)
+    @sound_lists_per_server[server_id]
+  end
+
+  def is_soundboard_configured?(server_id)
+    !@sound_lists_per_server[server_id].nil?
+  end
+
+  def start_threads
+    Thread.new do
+      while true
+        sleep 10
+    
+        @bot.voices.each do |server_id, voice|
+          if voice.channel.users.none? {|user| user.id != @bot.profile.id}
+            @currently_playing.delete(server_id)
+            voice.destroy
+          end
+        end
+      end
+    end
+
+    Thread.new do
+      while true
+        sleep(1)
+
+        @currently_playing.each do |server_id, sound|
+          if !@bot.voices[server_id]&.playing?
+            @bot.voices[server_id]&.play_file(sound.file)
+          end
+        end
+      end
+    end
+  end
+end
--- test.rb
+++ test.rb
@@ -5,24 +5,11 @@ require "discordrb"
 require "open-uri"
 require_relative "lib/config"
 require_relative "lib/sound"
+require_relative "lib/soundboard_manager"
 
 bot = Discordrb::Bot.new(token: DndMusic::Config.discord.token)
-
-sound_lists_per_server = {}
-currently_playing = {}
-
-Thread.new do
-  while true
-    sleep 10
-
-    bot.voices.each do |server_id, voice|
-      if voice.channel.users.none? {|user| user.id != bot.profile.id}
-        currently_playing.delete(server_id)
-        voice.destroy
-      end
-    end
-  end
-end
+soundboard_manager = SoundboardManager.new(bot: bot)
+soundboard_manager.start_threads
 
 bot.register_application_command(:soundboard, "Open the soundboard panel", server_id: "906610348726038529") do |soundboard_cmd|
   soundboard_cmd.subcommand(:controls, "Print the controls")
@@ -33,15 +20,16 @@ bot.register_application_command(:soundboard, "Open the soundboard panel", serve
   soundboard_cmd.subcommand(:play, "Play a sound") do |play_cmd|
     play_cmd.string("sound", "Which sound to play", required: true)
   end
+  soundboard_cmd.subcommand(:stop, "Stop the playback")
 end
 
 bot.application_command(:soundboard).subcommand(:controls) do |event|
-  next event.respond(content: "No list prepared", ephemeral: true) if !sound_lists_per_server[event.server.id]
+  next event.respond(content: "No list prepared", ephemeral: true) if !soundboard_manager.is_soundboard_configured?(event.server.id)
 
   event.respond(content: "Select a sound to start", ephemeral: true) do |_, view|
     view.row do |r|
       r.select_menu(custom_id: "sound_select", placeholder: "Select me!") do |s|
-        sound_lists_per_server[event.server.id].map do |sound|
+        soundboard_manager.get_soundboard(event.server.id).map do |sound|
           s.option(label: sound.name, value: sound.file)
         end
       end
@@ -59,15 +47,16 @@ bot.application_command(:soundboard).subcommand(:list) do |event|
   event.respond(content: sound_list, ephemeral: true)
 end
 bot.application_command(:soundboard).subcommand(:play) do |event|
+  sound = Sound.find(name: event.options["sound"])
+  next event.respond(content: "Sound not found: `#{event.options["sound"]}`") if !sound
+
   channel = event.user.voice_channel
   next event.respond(content: "You're not in any voice channel!") unless channel
   bot.voice_connect(channel)
 
-  sound = "sounds/#{event.options["sound"]}.mp3"
-  event.respond(content: "You selected: #{sound}", ephemeral: true)
+  event.respond(content: "You selected: #{sound.name}", ephemeral: true)
 
-  voice_bot = bot.voices[event.server.id]
-  voice_bot.play_file(sound)
+  soundboard_manager.play_sound(event.server.id, sound)
 end
 
 bot.application_command(:soundboard).subcommand(:configure) do |event|
@@ -79,7 +68,7 @@ bot.application_command(:soundboard).subcommand(:configure) do |event|
     data = f.read
   end
 
-  sounds = data.split("\n").map do |sound|
+  sounds = data.split(/\n|\r\n/).map do |sound|
     [sound, Sound.find(name: sound)]
   end
  
@@ -97,15 +86,23 @@ bot.application_command(:soundboard).subcommand(:configure) do |event|
 
   next event.respond(content: "Maximum of 25 sounds allowed", ephemeral: true) if sounds.count > 25
 
-  sound_lists_per_server[event.server.id] = sounds.map do |_name, sound|
-    sound
-  end
+  soundboard_manager.set_soundboard(
+    event.server.id,
+    sounds.map do |_name, sound|
+      sound
+    end
+  )
 
   response = <<~STR
     The soundboard has been configured with the following sounds:
     `#{sounds.map(&:first).join("`\n`")}`
   STR
   event.respond(content: response, ephemeral: true)
+end
+
+bot.application_command(:soundboard).subcommand(:stop) do |event|
+  soundboard_manager.stop(event.server.id)
+  event.respond(content: "Stopped the playback", ephemeral: true)
 end
 
 bot.select_menu(custom_id: "sound_select") do |event|
@@ -115,8 +112,7 @@ bot.select_menu(custom_id: "sound_select") do |event|
 
   event.respond(content: "You selected: #{event.values.first}", ephemeral: true)
 
-  voice_bot = bot.voices[event.server.id]
-  voice_bot.play_file(event.values.first)
+  soundboard_manager.play_sound(event.server.id, Sound.find(file: event.values.first))
 end
 
 bot.run
